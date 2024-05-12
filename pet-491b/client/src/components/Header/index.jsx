b91e773f0a9c3b656e9f8f8547297b23c1bf553f
DaV1005 <davidviers100598@gmail.com>
2024-05-12 00:07:43 -0700

    updated

--- pet-491b/client/src/components/Header/index.jsx
+++ pet-491b/client/src/components/Header/index.jsx
@@ -3,40 +3,39 @@ import { useTheme } from '../../components/ThemeContext.js'
 import user_icon from "./user_icon.webp"
 import logout from "./logout.jpg"
 import logo_for_website from "../../images/logo_for_website.png";
-import { useState } from "react";
-import  { useEffect } from "react";
+import { useState, useEffect } from "react";
 import React from "react";
-
+import { Link } from "react-router-dom";
 
 function Header() {
   const {isNightMode, toggleNightMode} = useTheme();
 
-   // Dynamically set the header class based on isNightMode
-   const headerClass = isNightMode ? styles.nightHeader : styles.header;
-   const navLink = isNightMode ? styles.nightNavItem : styles.navItem;
-   const toggle = isNightMode ? styles.lightModeToggle: styles.nightModeToggle;
+  // Dynamically set the header class based on isNightMode
+  const headerClass = isNightMode ? styles.nightHeader : styles.header;
+  const navLink = isNightMode ? styles.nightNavItem : styles.navItem;
+  const toggle = isNightMode ? styles.lightModeToggle : styles.nightModeToggle;
 
-   const handleLogout = () => {
-		console.log("logout");
-		window.open("http://localhost:8080/auth/logout", "_self");
-		localStorage.removeItem("token");
-		localStorage.removeItem("email");
-        localStorage.removeItem("name");
-		window.location = "/login";
-	};
+  const handleLogout = () => {
+    console.log("logout");
+    window.open("https://pet-adoption-matching.onrender.com/auth/logout", "_self");
+    localStorage.removeItem("token");
+    localStorage.removeItem("email");
+    localStorage.removeItem("name");
+    window.location = "/login";
+  };
 
   return (
     <div className={styles.top_bar}>
       <header className={headerClass}>
         {/* Logo */}
         <div className={styles.img_logo}>
-          <a href="/home" className={styles.logoLink}>
+          <Link to="/home" className={styles.logoLink}>
             <img
               className={styles.logo}
               src={logo_for_website}
               alt="Pet Adoption Match Logo"
             />
-          </a>
+          </Link>
         </div>
 
         <div className={styles.nav_bar}>
@@ -45,45 +44,43 @@ function Header() {
             <ul className={styles.navList}>
               {/* Enclose nav items in a ul for semantic structure */}
               <li className={navLink}>
-                <a href="/home">Home</a>
+                <Link to="/home">Home</Link>
               </li>
               <li className={navLink}>
-                <a href="/about">About Us</a>
+                <Link to="/about">About Us</Link>
               </li>
               <li className={navLink}>
-                <a href="/policy">Privacy Policy</a>
+                <Link to="/policy">Privacy Policy</Link>
               </li>
               <li className={navLink}>
-                <a href="/petCare">Pet Care Providers</a>
+                <Link to="/petCare">Pet Care Providers</Link>
               </li>
               <li className={navLink}>
-                <a href="/matching">Matching</a>
+                <Link to="/matching">Matching</Link>
               </li>
               <li className={navLink}>
-                <a href="/donate">Donate Now</a>
+                <Link to="/donate">Donate Now</Link>
               </li>
             </ul>
           </nav>
         </div>
 
         <div className={styles.iconsContainer}>
-          <a href="/userprofile" className={styles.iconLink}>
+          <Link to="/userprofile" className={styles.iconLink}>
             <img
               className={styles.userIcon}
               src={user_icon}
               alt="User Profile"
             />
-          </a>
+          </Link>
         </div>
 
         {/* Logout button */}
-        <div >
+        <div>
           <button onClick={handleLogout}>
-            <img className={styles.userIcon} src={logout} alt="my image" onClick={handleLogout}/>
+            <img className={styles.userIcon} src={logout} alt="Logout" onClick={handleLogout}/>
           </button>
         </div>
-
-
 
         {/* Night Mode Toggle Button */}
         <button
